ccccd247fa416248366038bfcdd66798873451e8
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-06 22:15:29 -0500

    t4 precision

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,16 +13,18 @@ import matplotlib.pyplot as plt
 def frequency():
     """Plots a histogram of student grades for Project A."""
     np.random.seed(5)
-    student_grades = np.random.normal(68, 15, 50)  # Mean = 68, std dev = 15, 50 students
+    student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
     # Plot the histogram with bins every 10 units and black outlines for the bars
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 
-    # Add labels and title
+    # Set the x-axis and y-axis labels
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
+
+    # Set the title
     plt.title("Project A")
 
-    # Show the plot
+    # Display the plot
     plt.show()
