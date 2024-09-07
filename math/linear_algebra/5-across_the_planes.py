b03a63a6836effd81f11293be5eb19846a6d3e70
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-07 02:07:10 +0000

    t5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module 5-across_the_planes
+This module contains a function that adds two 2D matrices element-wise.
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Adds two 2D matrices element-wise.
+
+    Args:
+        mat1: A 2D list of ints/floats.
+        mat2: A 2D list of ints/floats.
+
+    Returns:
+        A new 2D list with element-wise sums of mat1 and mat2, or
+        None if the matrices have different shapes.
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+
+    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
