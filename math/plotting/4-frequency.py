c6ffe3f3379feaf0392306d96d0531d191d9d750
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-06 22:20:37 -0500

    test t2

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -9,7 +9,6 @@ The x-axis represents grades, and the y-axis represents the number of students.
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def frequency():
     """Plots a histogram of student grades for Project A."""
     np.random.seed(5)
@@ -17,7 +16,7 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
 
     # Plot the histogram with bins every 10 units and black outlines for the bars
-    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black', range=(0, 100))
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 
     # Set the x-axis and y-axis labels
     plt.xlabel("Grades")
