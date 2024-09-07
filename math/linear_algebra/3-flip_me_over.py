e465a48b1de0b49adebf242f3255a761e6b35546
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-07 01:56:20 +0000

    t3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+Module 3-flip_me_over
+This module contains a function that returns
+the transpose of a 2D matrix.
+"""
+
 def matrix_transpose(matrix):
     """
     Returns the transpose of a 2D matrix.
