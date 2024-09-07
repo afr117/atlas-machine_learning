c514a82e216eb2c2d2394d28322d2d25ac2d77db
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-07 01:22:49 +0000

    file loads

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 =  # your code here
-arr2 =  # your code here
-arr3 =  # your code here
+arr1 = arr[:2]  # Slicing the first two elements
+arr2 = arr[-5:]  # Slicing the last five elements
+arr3 = arr[1:6]  # Slicing the 2nd to 6th elements (index 1 to 5)
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
