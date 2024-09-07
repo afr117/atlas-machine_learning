b6481d9a4cebb37580952e55bf5a90aa7ce594f8
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-07 02:27:10 +0000

    t10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 10-ill_use_my_scale
+This module contains a function that calculates the shape of a NumPy array.
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a NumPy array.
+
+    Args:
+        matrix: A NumPy ndarray.
+
+    Returns:
+        A tuple of integers representing the shape of the array.
+    """
+    return matrix.shape
