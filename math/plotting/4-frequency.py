458c7c8e6f7131952eb5db6a395249a016be8ea6
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-06 22:22:31 -0500

    t

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -16,7 +16,7 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
 
     # Plot the histogram with bins every 10 units and black outlines for the bars
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
 
     # Set the x-axis and y-axis labels
     plt.xlabel("Grades")
