55ef3aad33789ece153e255b1e1f545c163dce66
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-07 02:47:51 +0000

    t0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """Plots y = x^3 as a solid red line from x = 0 to 10."""
+    y = np.arange(0, 11) ** 3
+    x = np.arange(0, 11)
+    
+    plt.plot(x, y, color='red', linestyle='-')
+    plt.xlim(0, 10)  # Set x-axis range
+    plt.show()
