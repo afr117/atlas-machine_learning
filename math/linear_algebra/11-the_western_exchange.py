33243e70dc434e9f83076c9f445f758c4471ff23
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-07 02:29:15 +0000

    t11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 11-the_western_exchange
+This module contains a function that transposes a NumPy array.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a NumPy array.
+
+    Args:
+        matrix: A NumPy ndarray.
+
+    Returns:
+        A new ndarray representing the transposed matrix.
+    """
+    return matrix.T
