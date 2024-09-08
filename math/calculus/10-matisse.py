4a710cf3af4840683fc608ed02e2ed7b3453e127
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-08 00:05:03 +0000

    t10 pycode

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -23,7 +23,8 @@ def poly_derivative(poly):
         >>> poly_derivative([5, 3, 0, 1])
         [3, 0, 3]
     """
-    if not isinstance(poly, list) or len(poly) == 0 or not all(isinstance(coef, (int, float)) for coef in poly):
+    if not isinstance(poly, list) or len(poly) == 0 or not all(
+            isinstance(coef, (int, float)) for coef in poly):
         return None
     if len(poly) == 1:
         return [0]
