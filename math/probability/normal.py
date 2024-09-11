c5dd413a41f541f9111f2504333e2e9b4803d4d5
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-11 20:52:45 +0000

    t7

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -28,3 +28,23 @@ class Normal:
             self.mean = sum(data) / len(data)
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = variance ** 0.5
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value.
+        Args:
+            x: The x-value
+        Returns:
+            The z-score of x
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score.
+        Args:
+            z: The z-score
+        Returns:
+            The x-value of z
+        """
+        return z * self.stddev + self.mean
