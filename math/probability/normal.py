cde6ade9d148bbcf1506d4600a9c6a58611e676a
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-11 21:08:31 +0000

    t8, 9, 10, 11, 12

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -30,21 +30,36 @@ class Normal:
             self.stddev = variance ** 0.5
 
     def z_score(self, x):
-        """
-        Calculates the z-score of a given x-value.
-        Args:
-            x: The x-value
-        Returns:
-            The z-score of x
-        """
+        """Calculates the z-score of a given x-value."""
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
-        """
-        Calculates the x-value of a given z-score.
-        Args:
-            z: The z-score
-        Returns:
-            The x-value of z
-        """
+        """Calculates the x-value of a given z-score."""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value."""
+        pi = 3.1415926536
+        e = 2.7182818285
+        numerator = e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
+        denominator = self.stddev * ((2 * pi) ** 0.5)
+        return numerator / denominator
+
+    def erf(self, z):
+        """
+        Approximate the error function for a given z.
+        """
+        pi = 3.1415926536
+        term = (2 / (pi ** 0.5)) * (z - (z ** 3) / 3 + (z ** 5) / 10 - (z ** 7) / 42 + (z ** 9) / 216)
+        return term
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given x-value.
+        Args:
+            x: The x-value
+        Returns:
+            The CDF value for x
+        """
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        return 0.5 * (1 + self.erf(z))
