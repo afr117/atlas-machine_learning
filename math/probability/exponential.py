93c02ab564e82590dafd24a7629bd001b27ce9ae
afr117 <54492982+afr117@users.noreply.github.com>
2024-09-11 17:21:11 -0400

    Update exponential.py

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -23,7 +23,9 @@ class Exponential:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = 1 / (sum(data) / len(data))  # Calculate lambtha from data
+            # Break the long line
+            mean_data = sum(data) / len(data)
+            self.lambtha = 1 / mean_data  # Calculate lambtha from data
 
     def pdf(self, x):
         """
