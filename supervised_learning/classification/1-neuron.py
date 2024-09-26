937a11bb0470ccb71c7a20d0de5c81f1065086a2
afr117 <54492982+afr117@users.noreply.github.com>
2024-09-26 00:03:39 -0400

    Update 1-neuron.py

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -31,7 +31,8 @@ class Neuron:
             raise ValueError("nx must be a positive integer")
 
         # Initialize weights, bias, and activation output
-        self.__W = np.random.randn(1, nx)  # Weights initialized random values from normal distribution
+        self.__W = np.random.randn(1, nx)  
+        # Weights initialized random values from normal distribution
         self.__b = 0                       # Bias initialized to zero
         self.__A = 0                       # Activation output initialized to zero
 
