c68aa4f9c48f4672ca692aa1d87d4566e8598be1
afr117 <alfredo.figueroa07@yahoo.com>
2024-09-27 01:37:35 +0000

    t8

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -3,20 +3,17 @@ import numpy as np
 
 
 class NeuralNetwork:
-    """Defines a neural network with one hidden layer performing binary classification"""
+    """Defines a neural network with one hidden layer performing binary classification."""
 
     def __init__(self, nx, nodes):
         """
-        Class constructor
-        Args:
-            nx (int): Number of input features
-            nodes (int): Number of nodes in the hidden layer
-        Raises:
-            TypeError: If nx is not an integer
-            ValueError: If nx is less than 1
-            TypeError: If nodes is not an integer
-            ValueError: If nodes is less than 1
+        Initializes the NeuralNetwork.
+        
+        Arguments:
+        nx -- number of input features
+        nodes -- number of nodes in the hidden layer
         """
+
         # Validate nx
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -29,12 +26,12 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        # Initialize weights and biases for the hidden layer
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
+        # Initialize weights and biases for the hidden layer (no loop required)
+        self.W1 = np.random.randn(nodes, nx)  # Random normal initialization
+        self.b1 = np.zeros((nodes, 1))  # Bias initialized to zeros
+        self.A1 = 0  # Activated output initialized to 0
 
-        # Initialize weights and biases for the output layer
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
+        # Initialize weights and biases for the output layer (no loop required)
+        self.W2 = np.random.randn(1, nodes)  # Random normal initialization
+        self.b2 = np.zeros((1, 1))  # Bias initialized to zeros
+        self.A2 = 0  # Activated output initialized to 0
