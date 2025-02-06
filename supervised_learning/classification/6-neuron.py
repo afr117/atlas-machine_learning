e4705e6d8e77c3f5f6dc5b3ec81f42af857b0c45
afr117 <54492982+afr117@users.noreply.github.com>
2025-02-06 13:26:11 -0500

    Update 6-neuron.py

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import numpy as np
 
 class Neuron:
@@ -32,7 +33,7 @@ class Neuron:
     def sigmoid(self, Z):
         """Sigmoid activation function"""
         return 1 / (1 + np.exp(-Z))
-
+    
     def forward_prop(self, X):
         """Perform forward propagation to calculate activation"""
         Z = np.dot(self.__W, X) + self.__b
@@ -57,7 +58,7 @@ class Neuron:
         """Update weights and bias using gradient descent"""
         self.__W -= alpha * dW
         self.__b -= alpha * db
-
+    
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """Train the neuron"""
         # Validate input
@@ -94,37 +95,34 @@ class Neuron:
         predictions = np.round(A)
         return predictions, cost
 
-
-# Main code to test
-
-Neuron = __import__('6-neuron').Neuron
-
-# Loading training data
-lib_train = np.load('data/Binary_Train.npz')
-X_train_3D, Y_train = lib_train['X'], lib_train['Y']
-X_train = X_train_3D.reshape((X_train_3D.shape[0], -1)).T
-
-# Loading development data
-lib_dev = np.load('data/Binary_Dev.npz')
-X_dev_3D, Y_dev = lib_dev['X'], lib_dev['Y']
-X_dev = X_dev_3D.reshape((X_dev_3D.shape[0], -1)).T
-
-# Initializing the neuron
-np.random.seed(0)
-neuron = Neuron(X_train.shape[0])
-
-# Training the neuron with 5000 iterations
-A, cost = neuron.train(X_train, Y_train, iterations=5000, alpha=0.05)
-accuracy = np.sum(A == Y_train) / Y_train.shape[1] * 100
-print("Train cost:", np.round(cost, decimals=10))
-print("Train accuracy: {}%".format(np.round(accuracy, decimals=10)))
-print("Train data:", np.round(A, decimals=10))
-print("Train Neuron A:", np.round(neuron.A, decimals=10))
-
-# Evaluating the neuron on dev data
-A, cost = neuron.evaluate(X_dev, Y_dev)
-accuracy = np.sum(A == Y_dev) / Y_dev.shape[1] * 100
-print("Dev cost:", np.round(cost, decimals=10))
-print("Dev accuracy: {}%".format(np.round(accuracy, decimals=10)))
-print("Dev data:", np.round(A, decimals=10))
-print("Dev Neuron A:", np.round(neuron.A, decimals=10))
+# Main code to test the neuron
+if __name__ == '__main__':
+    # Loading training data
+    lib_train = np.load('data/Binary_Train.npz')
+    X_train_3D, Y_train = lib_train['X'], lib_train['Y']
+    X_train = X_train_3D.reshape((X_train_3D.shape[0], -1)).T
+    
+    # Loading development data
+    lib_dev = np.load('data/Binary_Dev.npz')
+    X_dev_3D, Y_dev = lib_dev['X'], lib_dev['Y']
+    X_dev = X_dev_3D.reshape((X_dev_3D.shape[0], -1)).T
+    
+    # Initializing the neuron
+    np.random.seed(0)
+    neuron = Neuron(X_train.shape[0])
+    
+    # Training the neuron with 5000 iterations
+    A, cost = neuron.train(X_train, Y_train, iterations=5000, alpha=0.05)
+    accuracy = np.sum(A == Y_train) / Y_train.shape[1] * 100
+    print("Train cost:", np.round(cost, decimals=10))
+    print("Train accuracy: {}%".format(np.round(accuracy, decimals=10)))
+    print("Train data:", np.round(A, decimals=10))
+    print("Train Neuron A:", np.round(neuron.A, decimals=10))
+    
+    # Evaluating the neuron on dev data
+    A, cost = neuron.evaluate(X_dev, Y_dev)
+    accuracy = np.sum(A == Y_dev) / Y_dev.shape[1] * 100
+    print("Dev cost:", np.round(cost, decimals=10))
+    print("Dev accuracy: {}%".format(np.round(accuracy, decimals=10)))
+    print("Dev data:", np.round(A, decimals=10))
+    print("Dev Neuron A:", np.round(neuron.A, decimals=10))
