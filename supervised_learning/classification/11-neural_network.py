ef00e95b2317e9181021f690944593847af16e58
afr117 <54492982+afr117@users.noreply.github.com>
2025-02-06 12:39:36 -0500

    Update 11-neural_network.py

--- supervised_learning/classification/11-neural_network.py
+++ supervised_learning/classification/11-neural_network.py
@@ -16,20 +16,20 @@ class NeuralNetwork:
             raise ValueError("nodes must be a positive integer")
         
         # Initialize parameters (weights and biases)
-        self.W1 = np.random.randn(nodes, nx)  # Weights for the hidden layer
-        self.b1 = np.zeros((nodes, 1))  # Bias for the hidden layer
-        self.A1 = np.zeros((nodes, 1))  # Activated output for the hidden layer
+        self.W1 = np.random.randn(nodes, nx)  # Weights of the hidden layer
+        self.b1 = np.zeros((nodes, 1))  # Bias of the hidden layer
+        self.A1 = np.zeros((nodes, 1))  # Activated output of the hidden layer
         
-        self.W2 = np.random.randn(1, nodes)  # Weights for the output layer
-        self.b2 = np.zeros((1, 1))  # Bias for the output layer
-        self.A2 = np.zeros((1, 1))  # Activated output for the output neuron
+        self.W2 = np.random.randn(1, nodes)  # Weights of the output layer
+        self.b2 = np.zeros((1, 1))  # Bias of the output layer
+        self.A2 = np.zeros((1, 1))  # Activated output of the output neuron
 
     def forward_prop(self, X):
-        # Z1 is the linear component for the hidden layer
+        # Z1 is the linear component of the hidden layer
         Z1 = np.dot(self.W1, X) + self.b1
         self.A1 = 1 / (1 + np.exp(-Z1))  # Sigmoid activation
         
-        # Z2 is the linear component for the output layer
+        # Z2 is the linear component of the output layer
         Z2 = np.dot(self.W2, self.A1) + self.b2
         self.A2 = 1 / (1 + np.exp(-Z2))  # Sigmoid activation
         
