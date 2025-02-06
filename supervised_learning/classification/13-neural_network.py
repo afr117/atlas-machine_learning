a3f754bda51c70e4ac7c900c8b9ea19198e73839
afr117 <54492982+afr117@users.noreply.github.com>
2025-02-06 12:43:35 -0500

    Update 13-neural_network.py

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -13,21 +13,21 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         
-        # Initialize the weights, biases, and activated outputs for both layers
-        self.W1 = np.random.randn(nodes, nx)  # Weights for the hidden layer
-        self.b1 = np.zeros((nodes, 1))  # Bias for the hidden layer
-        self.A1 = np.zeros((nodes, 1))  # Activated output for the hidden layer
+        # Initialize the weights, biases, and activated outputs of both layers
+        self.W1 = np.random.randn(nodes, nx)  # Weights of the hidden layer
+        self.b1 = np.zeros((nodes, 1))  # Bias of the hidden layer
+        self.A1 = np.zeros((nodes, 1))  # Activated output of the hidden layer
         
-        self.W2 = np.random.randn(1, nodes)  # Weights for the output layer
-        self.b2 = np.zeros((1, 1))  # Bias for the output layer
-        self.A2 = np.zeros((1, 1))  # Activated output for the output layer
+        self.W2 = np.random.randn(1, nodes)  # Weights of the output layer
+        self.b2 = np.zeros((1, 1))  # Bias of the output layer
+        self.A2 = np.zeros((1, 1))  # Activated output of the output layer
     
     def forward_prop(self, X):
-        Z1 = np.dot(self.W1, X) + self.b1  # Linear transformation for the hidden layer
-        self.A1 = 1 / (1 + np.exp(-Z1))  # Sigmoid activation function for the hidden layer
+        Z1 = np.dot(self.W1, X) + self.b1  # Linear transformation of the hidden layer
+        self.A1 = 1 / (1 + np.exp(-Z1))  # Sigmoid activation function of the hidden layer
         
-        Z2 = np.dot(self.W2, self.A1) + self.b2  # Linear transformation for the output layer
-        self.A2 = 1 / (1 + np.exp(-Z2))  # Sigmoid activation function for the output layer
+        Z2 = np.dot(self.W2, self.A1) + self.b2  # Linear transformation of the output layer
+        self.A2 = 1 / (1 + np.exp(-Z2))  # Sigmoid activation function of the output layer
         
         return self.A1, self.A2
     
