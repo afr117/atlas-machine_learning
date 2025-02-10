148161b64b61613694878d5ccdd87fbbcb55cec5
afr117 <54492982+afr117@users.noreply.github.com>
2025-02-09 20:58:00 -0500

    Update 26-deep_neural_network.py

---
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pickle
+
+class DeepNeuralNetwork:
+    """Defines a deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """Class constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if any(map(lambda l: not isinstance(l, int) or l <= 0, layers)):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        def initialize_weights(index, prev_layer):
+            if index > self.__L:
+                return
+            self.__weights[f"W{index}"] = (
+                np.random.randn(layers[index - 1], prev_layer) * np.sqrt(2 / prev_layer)
+            )
+            self.__weights[f"b{index}"] = np.zeros((layers[index - 1], 1))
+            initialize_weights(index + 1, layers[index - 1])
+        
+        initialize_weights(1, nx)
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
+
+    def save(self, filename):
+        """Saves the instance object to a file in pickle format"""
+        if not filename.endswith(".pkl"):
+            filename += ".pkl"
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def load(filename):
+        """Loads a pickled DeepNeuralNetwork object"""
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except FileNotFoundError:
+            return None
