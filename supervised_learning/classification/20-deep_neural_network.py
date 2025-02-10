ad43c0757226580441d29d599eb71132ecc19189
afr117 <54492982+afr117@users.noreply.github.com>
2025-02-09 22:43:45 -0500

    Update 20-deep_neural_network.py

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -47,8 +47,8 @@ class DeepNeuralNetwork:
 
     def forward_prop(self, X):
         """Calculates forward propagation of the deep neural network"""
-        self.__cache["A0"] = X
         
+        self.__cache["A0"] = X
         def activate(layer):
             if layer > self.__L:
                 return self.__cache[f"A{self.__L}"]
