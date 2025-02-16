1fc9daa5e009b4920ca59dbb0f87e3ad14e3744c
afr117 <54492982+afr117@users.noreply.github.com>
2025-02-16 18:30:15 -0500

    Update 1-normalize.py

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -4,16 +4,20 @@ Normalizes (standardizes) a matrix using given mean and standard deviation.
 """
 import numpy as np
 
+
 def normalize(X, m, s):
     """
     Normalizes a matrix.
-    
+
     Parameters:
-    X (numpy.ndarray): A matrix of shape (d, nx) where d is the number of data points
+    X (numpy.ndarray): A matrix of shape (d, nx)
+    where d is the number of data points
                        and nx is the number of features.
-    m (numpy.ndarray): A 1D array of shape (nx,) containing the mean of each feature.
-    s (numpy.ndarray): A 1D array of shape (nx,) containing the standard deviation of each feature.
-    
+    m (numpy.ndarray): A 1D array of shape (nx,)
+    containing the mean of each feature.
+    s (numpy.ndarray): A 1D array of shape (nx,)
+    containing the standard deviation of each feature.
+
     Returns:
     numpy.ndarray: The normalized matrix.
     """
