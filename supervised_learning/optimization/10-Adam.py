a26a515630992c3533ff7e7be493ead0d4aba8dd
afr117 <54492982+afr117@users.noreply.github.com>
2025-02-16 18:47:52 -0500

    Update 10-Adam.py

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -21,4 +21,5 @@ def create_Adam_op(alpha, beta1, beta2, epsilon):
     The optimizer configured with Adam.
     """
     return tf.keras.optimizers.Adam(learning_rate=alpha,
-                                    beta_1=beta1, beta_2=beta2, epsilon=epsilon)
+                                    beta_1=beta1, beta_2=beta2,
+                                    epsilon=epsilon)
