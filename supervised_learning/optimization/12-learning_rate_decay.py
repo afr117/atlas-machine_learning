961b2358af282fef2f7fca83ecd1b52425e2c5a3
afr117 <54492982+afr117@users.noreply.github.com>
2025-02-16 18:51:00 -0500

    Update 12-learning_rate_decay.py

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Creates a learning rate decay operation in TensorFlow using inverse time decay.
+"""
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """
+    Creates a learning rate decay operation in
+    TensorFlow using inverse time decay.
+
+    Parameters:
+    alpha (float): The original learning rate.
+    decay_rate (float): The weight used to
+    determine the rate at which alpha will decay.
+    decay_step (int): The number of passes of gradient
+    descent that should occur before alpha is decayed further.
+
+    Returns:
+    tensorflow.keras.optimizers.schedules.InverseTimeDecay:
+    The learning rate decay operation.
+    """
+    return tf.keras.optimizers.schedules.InverseTimeDecay(initial_learning_rate=alpha,
+                                                          decay_steps=decay_step,
+                                                          decay_rate=decay_rate,
+                                                          staircase=True)
