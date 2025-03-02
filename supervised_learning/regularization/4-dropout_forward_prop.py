7d7602f6b3bd5a0f7dda8e65e6989af74783b4ec
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-02 13:13:43 -0500

    Update 4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -2,28 +2,30 @@
 """
 Conducts forward propagation using Dropout.
 """
+
 import numpy as np
+
 
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
     Conducts forward propagation using Dropout.
-    
+
     Args:
         X (numpy.ndarray): Input data for the network of shape (nx, m).
         weights (dict): Dictionary containing the weights and biases of the network.
         L (int): Number of layers in the network.
         keep_prob (float): Probability that a node will be kept.
-    
+
     Returns:
         dict: Dictionary containing the outputs of each layer and dropout masks.
     """
     cache = {'A0': X}
-    
+
     for i in range(1, L + 1):
         W = weights[f'W{i}']
         b = weights[f'b{i}']
         Z = np.matmul(W, cache[f'A{i-1}']) + b
-        
+
         if i == L:
             # Softmax activation for the last layer
             exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
@@ -35,7 +37,7 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             A *= D
             A /= keep_prob
             cache[f'D{i}'] = D.astype(int)  # Ensure dropout mask is binary (0/1)
-        
+
         cache[f'A{i}'] = A
-    
+
     return cache
