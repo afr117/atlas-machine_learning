97371cb92d6e014640059596e16f401ed313d11e
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-02 15:01:28 -0500

    Update 7-train.py

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -17,17 +17,27 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
     Args:
         network (keras.Model): The model to train.
         data (numpy.ndarray): Input data of shape (m, nx).
-        labels (numpy.ndarray): One-hot labels of shape (m, classes).
-        batch_size (int): Batch size for mini-batch gradient descent.
+        labels (numpy.ndarray): One-hot labels
+        of shape (m, classes).
+        batch_size (int): Batch size for
+        mini-batch gradient descent.
         epochs (int): Number of training epochs.
-        validation_data (tuple, optional): Data to validate the model with. Defaults to None.
-        early_stopping (bool, optional): Whether to apply early stopping. Defaults to False.
-        patience (int, optional): Number of epochs to wait before stopping if no improvement. Defaults to 0.
-        learning_rate_decay (bool, optional): Whether to use learning rate decay. Defaults to False.
-        alpha (float, optional): Initial learning rate. Defaults to 0.1.
-        decay_rate (float, optional): Decay rate for learning rate. Defaults to 1.
-        verbose (bool, optional): Whether to print training output. Defaults to True.
-        shuffle (bool, optional): Whether to shuffle data every epoch. Defaults to False.
+        validation_data (tuple, optional): Data to validate the model with.
+        Defaults to None.
+        early_stopping (bool, optional): Whether to apply early stopping.
+        Defaults to False.
+        patience (int, optional): Number of epochs to wait before
+        stopping if no improvement. Defaults to 0.
+        learning_rate_decay (bool, optional): Whether to use learning rate decay.
+        Defaults to False.
+        alpha (float, optional): Initial learning rate.
+        Defaults to 0.1.
+        decay_rate (float, optional): Decay rate for learning rate.
+        Defaults to 1.
+        verbose (bool, optional): Whether to print training output.
+        Defaults to True.
+        shuffle (bool, optional): Whether to shuffle data every epoch.
+        Defaults to False.
 
     Returns:
         keras.callbacks.History: The history object generated after training.
@@ -35,7 +45,8 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
     callbacks = []
 
     if early_stopping and validation_data is not None:
-        early_stop = K.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
+        early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
+                                               patience=patience)
         callbacks.append(early_stop)
 
     if learning_rate_decay and validation_data is not None:
