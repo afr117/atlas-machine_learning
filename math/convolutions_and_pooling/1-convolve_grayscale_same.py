8038149a8f46a85cdedb569eead963ec511e064f
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-02 18:21:48 -0500

    Update 1-convolve_grayscale_same.py

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -11,16 +11,19 @@ def convolve_grayscale_same(images, kernel):
     Performs a same convolution on grayscale images.
 
     Parameters:
-    images (numpy.ndarray): A numpy array of shape (m, h, w) containing multiple grayscale images.
+    images (numpy.ndarray): A numpy array of shape (m, h, w)
+    containing multiple grayscale images.
         - m: Number of images.
         - h: Height of images in pixels.
         - w: Width of images in pixels.
-    kernel (numpy.ndarray): A numpy array of shape (kh, kw) containing the kernel for convolution.
+    kernel (numpy.ndarray): A numpy array of shape (kh, kw)
+    containing the kernel for convolution.
         - kh: Height of the kernel.
         - kw: Width of the kernel.
 
     Returns:
-    numpy.ndarray: A numpy array containing the convolved images with the same spatial dimensions.
+    numpy.ndarray: A numpy array containing the convolved
+    images with the same spatial dimensions.
     """
     # Retrieve dimensions
     m, h, w = images.shape
@@ -31,7 +34,8 @@ def convolve_grayscale_same(images, kernel):
     pad_w = kw // 2
 
     # Pad the images with zeros
-    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
+    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h),
+                                    (pad_w, pad_w)), mode='constant')
 
     # Initialize output array
     output = np.zeros((m, h, w))
@@ -39,6 +43,7 @@ def convolve_grayscale_same(images, kernel):
     # Perform same convolution using exactly two for-loops
     for i in range(h):
         for j in range(w):
-            output[:, i, j] = np.sum(padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+            output[:, i, j] = np.sum(padded_images[:, i:i+kh,
+                                     j:j+kw] * kernel, axis=(1, 2))
 
     return output
