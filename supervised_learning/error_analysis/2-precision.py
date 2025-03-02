4a4ec8ebcb4248d6d7ef6a8142e99531d4cfc66f
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-01 19:48:21 -0500

    Update 2-precision.py

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module to calculate precision for each class in a confusion matrix."""
+"""Module to calculate precision for each class in
+a confusion matrix."""
 
 import numpy as np
 
@@ -18,5 +19,6 @@ def precision(confusion):
                        of each class.
     """
     true_positives = np.diag(confusion)  # Extract the diagonal (TP)
-    false_positives = np.sum(confusion, axis=0) - true_positives  # Sum of column - TP
+    false_positives = np.sum(confusion, axis=0) - true_positives 
+    # Sum of column - TP
     return true_positives / (true_positives + false_positives)
