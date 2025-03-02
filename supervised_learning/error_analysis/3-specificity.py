2fc2062937359f93b39c16c887a2b3aebeee9ce4
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-01 19:52:10 -0500

    Update 3-specificity.py

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Module to calculate specificity for each class in
-a confusion matrix."""
+"""Module to calculate specificity for each class in a confusion matrix."""
 
 import numpy as np
 
@@ -15,17 +14,12 @@ def specificity(confusion):
                                    columns represent the predicted labels.
 
     Returns:
-        numpy.ndarray: A 1D array of shape (classes,)
-        containing the specificity
+        numpy.ndarray: A 1D array of shape (classes,) containing the specificity
                        of each class.
     """
-    true_positives = np.diag(confusion)
-  # Extract the diagonal (TP)
-    false_positives = np.sum(confusion, axis=0) - true_positives
-  # FP = Sum of column - TP
-    false_negatives = np.sum(confusion, axis=1) - true_positives
-  # FN = Sum of row - TP
-    true_negatives = np.sum(confusion) - (true_positives + false_positives
-                                          + false_negatives) # TN
+    true_positives = np.diag(confusion)  # Extract the diagonal (TP)
+    false_positives = np.sum(confusion, axis=0) - true_positives  # FP = Sum of column - TP
+    false_negatives = np.sum(confusion, axis=1) - true_positives  # FN = Sum of row - TP
+    true_negatives = np.sum(confusion) - (true_positives + false_positives + false_negatives)  # TN
 
     return true_negatives / (true_negatives + false_positives)
