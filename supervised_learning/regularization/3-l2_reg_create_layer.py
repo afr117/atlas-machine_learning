0854a065767cdacdf299877fd63401dee2c8cb61
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-02 13:04:44 -0500

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -19,6 +19,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         tf.Tensor: Output of the new layer.
     """
     regularizer = tf.keras.regularizers.L2(lambtha)
-    initializer = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
-    layer = tf.keras.layers.Dense(n, activation=activation, kernel_initializer=initializer, kernel_regularizer=regularizer)
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                        mode="fan_avg")
+    layer = tf.keras.layers.Dense(n, activation=activation,
+                                  kernel_initializer=initializer, kernel_regularizer=regularizer)
     return layer(prev)
