0361ea72248839fd675a26c80ff2bd988a56084b
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-18 19:23:05 -0400

    Update 1-pool_forward.py

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -4,6 +4,7 @@ Performs forward propagation over a pooling layer of a neural network.
 """
 import numpy as np
 
+
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
     Performs forward propagation over a pooling layer.
@@ -20,12 +21,12 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw = kernel_shape
     sh, sw = stride
-    
+
     output_h = (h_prev - kh) // sh + 1
     output_w = (w_prev - kw) // sw + 1
-    
+
     output = np.zeros((m, output_h, output_w, c_prev))
-    
+
     for i in range(output_h):
         for j in range(output_w):
             if mode == 'max':
@@ -38,5 +39,5 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
                     A_prev[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :],
                     axis=(1, 2)
                 )
-    
+
     return output
