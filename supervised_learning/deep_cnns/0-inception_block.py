582caf341ce401531a7fe50536319553c674d4d9
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-18 20:39:30 -0400

    Update 0-inception_block.py

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -22,26 +22,36 @@ def inception_block(A_prev, filters):
 
     # 1x1 Convolution Branch
     conv1x1 = K.layers.Conv2D(filters=F1, kernel_size=(1, 1), padding='same',
-                              activation='relu', kernel_initializer=he_normal)(A_prev)
+                              activation='relu',
+                              kernel_initializer=he_normal)(A_prev)
 
     # 1x1 Convolution -> 3x3 Convolution Branch
     conv3x3_reduce = K.layers.Conv2D(filters=F3R, kernel_size=(1, 1), padding='same',
-                                     activation='relu', kernel_initializer=he_normal)(A_prev)
+                                     activation='relu',
+                                     kernel_initializer=he_normal)(A_prev)
     conv3x3 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same',
-                              activation='relu', kernel_initializer=he_normal)(conv3x3_reduce)
+                              activation='relu',
+                              kernel_initializer=he_normal)(conv3x3_reduce)
 
     # 1x1 Convolution -> 5x5 Convolution Branch
-    conv5x5_reduce = K.layers.Conv2D(filters=F5R, kernel_size=(1, 1), padding='same',
-                                     activation='relu', kernel_initializer=he_normal)(A_prev)
+    conv5x5_reduce = K.layers.Conv2D(filters=F5R,
+                                     kernel_size=(1, 1), padding='same',
+                                     activation='relu',
+                                     kernel_initializer=he_normal)(A_prev)
     conv5x5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5), padding='same',
-                              activation='relu', kernel_initializer=he_normal)(conv5x5_reduce)
+                              activation='relu',
+                              kernel_initializer=he_normal)(conv5x5_reduce)
 
     # Max Pooling -> 1x1 Convolution Branch
-    max_pool = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding='same')(A_prev)
-    conv_pool = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1), padding='same',
-                                activation='relu', kernel_initializer=he_normal)(max_pool)
+    max_pool = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                     strides=(1, 1), padding='same')(A_prev)
+    conv_pool = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1),
+                                padding='same',
+                                activation='relu',
+                                kernel_initializer=he_normal)(max_pool)
 
     # Concatenation of all branches
-    output = K.layers.Concatenate(axis=-1)([conv1x1, conv3x3, conv5x5, conv_pool])
+    output = K.layers.Concatenate(axis=-1)([conv1x1, conv3x3,
+                                            conv5x5, conv_pool])
 
     return output
