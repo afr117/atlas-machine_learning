6c365c3da0f448373c49604c3249721522afb11a
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-20 10:43:37 -0400

    Update 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -21,27 +21,26 @@ def identity_block(A_prev, filters):
     - Activated output of the identity block.
     """
     F11, F3, F12 = filters
-    initializer = K.initializers.HeNormal(seed=0)  # He Normal Initialization with seed 0
+
+    # Define weight initializer
+    initializer = K.initializers.HeNormal(seed=0)
 
     # First 1x1 Convolution
-    X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), padding='same',
-                        kernel_initializer=initializer)(A_prev)
+    X = K.layers.Conv2D(F11, (1, 1), padding="same", kernel_initializer=initializer)(A_prev)
     X = K.layers.BatchNormalization(axis=3)(X)
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation("relu")(X)
 
     # 3x3 Convolution
-    X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same',
-                        kernel_initializer=initializer)(X)
+    X = K.layers.Conv2D(F3, (3, 3), padding="same", kernel_initializer=initializer)(X)
     X = K.layers.BatchNormalization(axis=3)(X)
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation("relu")(X)
 
     # Second 1x1 Convolution
-    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), padding='same',
-                        kernel_initializer=initializer)(X)
+    X = K.layers.Conv2D(F12, (1, 1), padding="same", kernel_initializer=initializer)(X)
     X = K.layers.BatchNormalization(axis=3)(X)
 
     # Add skip connection (A_prev must have the same shape as X)
     X = K.layers.Add()([X, A_prev])
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation("relu")(X)
 
     return X
