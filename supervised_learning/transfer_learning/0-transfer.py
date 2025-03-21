99b2e58a23dfa302bf8f7838330d8705b357fc9c
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-21 18:59:02 -0400

    Update 0-transfer.py

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python3
 """
-Trains a CNN on CIFAR-10 using transfer learning and MobileNetV2
+Trains a CNN on CIFAR-10 using transfer learning with MobileNetV2
 """
 from tensorflow import keras as K
-import numpy as np
+import tensorflow as tf
 
 def preprocess_data(X, Y):
     """
-    Pre-process the data for the model
+    Pre-processes the data for the model
     """
-    X = X.astype('float32')
-    X_resized = np.zeros((X.shape[0], 96, 96, 3), dtype='float32')
-    resize_layer = K.layers.Resizing(96, 96)
-    for i in range(0, X.shape[0], 1000):  # process in batches
-        X_resized[i:i+1000] = resize_layer(X[i:i+1000])
-    X_p = K.applications.mobilenet_v2.preprocess_input(X_resized)
-    Y_p = K.utils.to_categorical(Y, 10)
-    return X_p, Y_p
+    X = tf.image.resize(X, (96, 96))  # Resizes on the fly
+    X = K.applications.mobilenet_v2.preprocess_input(X)
+    Y = K.utils.to_categorical(Y, 10)
+    return X, Y
 
 if __name__ == '__main__':
-    # Load CIFAR-10
+    # Load and preprocess data
     (X_train, Y_train), (X_test, Y_test) = K.datasets.cifar10.load_data()
     X_train, Y_train = preprocess_data(X_train, Y_train)
     X_test, Y_test = preprocess_data(X_test, Y_test)
 
-    # Base model
-    base = K.applications.MobileNetV2(include_top=False,
-                                      input_shape=(96, 96, 3),
-                                      pooling='avg',
-                                      weights='imagenet')
-    base.trainable = False
+    # Build model with MobileNetV2
+    base_model = K.applications.MobileNetV2(include_top=False,
+                                            weights='imagenet',
+                                            input_shape=(96, 96, 3),
+                                            pooling='avg')
+    base_model.trainable = False
 
-    # Custom head
     inputs = K.Input(shape=(96, 96, 3))
-    x = base(inputs, training=False)
+    x = base_model(inputs, training=False)
     x = K.layers.Dense(256, activation='relu')(x)
     outputs = K.layers.Dense(10, activation='softmax')(x)
     model = K.Model(inputs, outputs)
@@ -44,7 +39,7 @@ if __name__ == '__main__':
 
     model.fit(X_train, Y_train,
               epochs=5,
-              batch_size=64,  # smaller batch size = lower memory usage
+              batch_size=64,
               validation_data=(X_test, Y_test),
               verbose=1)
 
