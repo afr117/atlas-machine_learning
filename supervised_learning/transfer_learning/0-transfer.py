1c02ff0cb05c4efdaa1252be8956b2ebd396a49b
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-21 18:52:19 -0400

    Update 0-transfer.py

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -1,40 +1,49 @@
 #!/usr/bin/env python3
-"""Trains a tiny CNN to classify CIFAR-10 and saves the model"""
+"""
+Trains a CNN on CIFAR-10 using transfer learning and MobileNetV2
+"""
 from tensorflow import keras as K
-
+import numpy as np
 
 def preprocess_data(X, Y):
-    """Pre-process CIFAR-10 data"""
-    X = X.astype('float32') / 255.0
-    Y = K.utils.to_categorical(Y, 10)
-    return X, Y
-
+    """
+    Pre-process the data for the model
+    """
+    X_p = K.applications.mobilenet_v2.preprocess_input(
+        K.layers.Resizing(96, 96)(X.astype('float32')))
+    Y_p = K.utils.to_categorical(Y, 10)
+    return X_p.numpy(), Y_p
 
 if __name__ == '__main__':
-    (X_train, Y_train), (X_val, Y_val) = K.datasets.cifar10.load_data()
+    # Load CIFAR-10
+    (X_train, Y_train), (X_test, Y_test) = K.datasets.cifar10.load_data()
     X_train, Y_train = preprocess_data(X_train, Y_train)
-    X_val, Y_val = preprocess_data(X_val, Y_val)
+    X_test, Y_test = preprocess_data(X_test, Y_test)
 
-    model = K.models.Sequential([
-        K.layers.Input(shape=(32, 32, 3)),
-        K.layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
-        K.layers.MaxPooling2D(),
-        K.layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-        K.layers.MaxPooling2D(),
-        K.layers.Flatten(),
-        K.layers.Dense(64, activation='relu'),
-        K.layers.Dropout(0.3),
-        K.layers.Dense(10, activation='softmax')
-    ])
+    # Load base model (frozen)
+    base_model = K.applications.MobileNetV2(include_top=False,
+                                            weights='imagenet',
+                                            input_shape=(96, 96, 3),
+                                            pooling='avg')
+    base_model.trainable = False
 
-    model.compile(optimizer='adam',
+    # Build model
+    inputs = K.Input(shape=(96, 96, 3))
+    x = base_model(inputs, training=False)
+    outputs = K.layers.Dense(10, activation='softmax')(x)
+    model = K.Model(inputs, outputs)
+
+    # Compile
+    model.compile(optimizer=K.optimizers.Adam(),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
+    # Train
     model.fit(X_train, Y_train,
-              epochs=5,
+              validation_data=(X_test, Y_test),
+              epochs=10,
               batch_size=128,
-              validation_data=(X_val, Y_val),
               verbose=1)
 
-    model.save('cifar10.h5')
+    # Save
+    model.save("cifar10.h5")
