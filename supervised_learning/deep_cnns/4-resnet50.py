515051c5ae73df97e3fb9e4c70bd0858afe69884
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-21 17:02:05 -0400

    Update 4-resnet50.py

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -3,8 +3,7 @@
 Builds the ResNet-50 architecture as described in
 "Deep Residual Learning for Image Recognition" (2015)
 """
-import tensorflow.keras as K
-
+from tensorflow import keras as K
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
 
