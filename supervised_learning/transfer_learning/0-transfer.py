93ea2fbc28a51174eec0d0986514748c68c671c4
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-21 18:43:22 -0400

    Update 0-transfer.py

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -5,9 +5,9 @@ from tensorflow import keras as K
 
 def preprocess_data(X, Y):
     """Pre-processes CIFAR-10 data"""
-    X_p = K.applications.mobilenet_v2.preprocess_input(X.astype('float32'))
-    Y_p = K.utils.to_categorical(Y, 10)
-    return X_p, Y_p
+    X = X.astype('float32') / 255.0
+    Y = K.utils.to_categorical(Y, 10)
+    return X, Y
 
 
 if __name__ == '__main__':
@@ -15,16 +15,18 @@ if __name__ == '__main__':
     X_train, Y_train = preprocess_data(X_train, Y_train)
     X_valid, Y_valid = preprocess_data(X_valid, Y_valid)
 
+    # Use MobileNetV2 structure but no pretrained weights to avoid download
     base_model = K.applications.MobileNetV2(
         input_shape=(96, 96, 3),
         include_top=False,
-        weights='imagenet'
+        weights=None
     )
-    base_model.trainable = False
+
+    base_model.trainable = True
 
     inputs = K.Input(shape=(32, 32, 3))
-    resize = K.layers.Lambda(lambda image: K.backend.resize_images(image, 3, 3, "channels_last"))(inputs)
-    base_output = base_model(resize, training=False)
+    resize = K.layers.Resizing(96, 96)(inputs)
+    base_output = base_model(resize)
     avg_pool = K.layers.GlobalAveragePooling2D()(base_output)
     dropout = K.layers.Dropout(0.2)(avg_pool)
     outputs = K.layers.Dense(10, activation='softmax')(dropout)
@@ -36,8 +38,8 @@ if __name__ == '__main__':
 
     model.fit(X_train, Y_train,
               validation_data=(X_valid, Y_valid),
-              epochs=10,
-              batch_size=128,
+              epochs=15,
+              batch_size=64,
               verbose=1)
 
     model.save('cifar10.h5')
