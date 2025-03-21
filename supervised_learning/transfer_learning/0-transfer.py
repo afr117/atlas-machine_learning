70f7a8aa40050ca79ed48d2d764b7bce90bb6ed8
afr117 <alfredo.figueroa07@yahoo.com>
2025-03-21 22:42:35 +0000

    test

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -1,63 +1,43 @@
 #!/usr/bin/env python3
-"""
-Trains a CNN with transfer learning on CIFAR-10 and saves the model.
-"""
-
+"""Trains a CNN using transfer learning to classify CIFAR-10"""
 from tensorflow import keras as K
-from tensorflow.keras import layers
-import numpy as np
 
 
 def preprocess_data(X, Y):
-    """
-    Preprocesses the CIFAR-10 data.
-
-    Args:
-        X: np.ndarray - shape (m, 32, 32, 3), image data.
-        Y: np.ndarray - shape (m, ), labels.
-
-    Returns:
-        X_p: preprocessed image data
-        Y_p: one-hot encoded labels
-    """
-    X_p = X.astype('float32') / 255.0
+    """Pre-processes CIFAR-10 data"""
+    X_p = K.applications.mobilenet_v2.preprocess_input(X.astype('float32'))
     Y_p = K.utils.to_categorical(Y, 10)
     return X_p, Y_p
 
 
-if __name__ == "__main__":
-    # Load and preprocess data
-    (X_train, Y_train), (X_test, Y_test) = K.datasets.cifar10.load_data()
+if __name__ == '__main__':
+    (X_train, Y_train), (X_valid, Y_valid) = K.datasets.cifar10.load_data()
     X_train, Y_train = preprocess_data(X_train, Y_train)
-    X_test, Y_test = preprocess_data(X_test, Y_test)
+    X_valid, Y_valid = preprocess_data(X_valid, Y_valid)
 
-    # Resize images to match input size of pretrained model
-    input_tensor = K.Input(shape=(32, 32, 3))
-    resize = layers.Lambda(lambda image: K.backend.resize_images(image, 7, 7, "channels_last"))(input_tensor)
-
-    # Load base model
-    base_model = K.applications.Xception(
+    base_model = K.applications.MobileNetV2(
+        input_shape=(96, 96, 3),
         include_top=False,
-        weights="imagenet",
-        input_tensor=resize,
-        pooling='avg'
+        weights='imagenet'
     )
-    base_model.trainable = False  # Freeze base model
+    base_model.trainable = False
 
-    # Add classification head
-    output = layers.Dense(10, activation='softmax')(base_model.output)
-    model = K.Model(inputs=input_tensor, outputs=output)
+    inputs = K.Input(shape=(32, 32, 3))
+    resize = K.layers.Lambda(lambda image: K.backend.resize_images(image, 3, 3, "channels_last"))(inputs)
+    base_output = base_model(resize, training=False)
+    avg_pool = K.layers.GlobalAveragePooling2D()(base_output)
+    dropout = K.layers.Dropout(0.2)(avg_pool)
+    outputs = K.layers.Dense(10, activation='softmax')(dropout)
 
-    # Compile and train
-    model.compile(optimizer=K.optimizers.Adam(),
+    model = K.Model(inputs, outputs)
+    model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     model.fit(X_train, Y_train,
-              batch_size=128,
+              validation_data=(X_valid, Y_valid),
               epochs=10,
-              validation_data=(X_test, Y_test),
+              batch_size=128,
               verbose=1)
 
-    # Save model
-    model.save("cifar10.h5")
+    model.save('cifar10.h5')
