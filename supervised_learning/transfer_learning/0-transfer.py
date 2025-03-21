41a0b45fd3c399a8087ed8b9d61af042565e56dc
afr117 <54492982+afr117@users.noreply.github.com>
2025-03-21 19:15:41 -0400

    Update 0-transfer.py

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -2,13 +2,13 @@
 """
 Transfer learning with MobileNetV2 on CIFAR-10
 """
-
 from tensorflow import keras as K
 import numpy as np
 
+
 def preprocess_data(X, Y):
     """
-    Preprocess the data by normalizing and one-hot encoding
+    Normalizes the data and converts labels to one-hot
     """
     X = X.astype('float32') / 255.0
     Y = K.utils.to_categorical(Y, 10)
@@ -16,25 +16,10 @@ def preprocess_data(X, Y):
 
 
 if __name__ == '__main__':
-    # Load CIFAR-10 dataset
+    # Load and preprocess data
     (X_train, Y_train), (X_test, Y_test) = K.datasets.cifar10.load_data()
     X_train, Y_train = preprocess_data(X_train, Y_train)
     X_test, Y_test = preprocess_data(X_test, Y_test)
-
-    # Resize on-the-fly using ImageDataGenerator
-    datagen = K.preprocessing.image.ImageDataGenerator(
-        preprocessing_function=K.applications.mobilenet_v2.preprocess_input,
-        validation_split=0.1
-    )
-
-    train_gen = datagen.flow(
-        X_train, Y_train, batch_size=64, subset='training', shuffle=True,
-        target_size=(96, 96)
-    )
-    val_gen = datagen.flow(
-        X_train, Y_train, batch_size=64, subset='validation', shuffle=False,
-        target_size=(96, 96)
-    )
 
     base_model = K.applications.MobileNetV2(
         input_shape=(96, 96, 3),
@@ -44,22 +29,26 @@ if __name__ == '__main__':
     )
     base_model.trainable = False
 
-    model = K.models.Sequential([
-        base_model,
-        K.layers.Dense(128, activation='relu'),
-        K.layers.Dropout(0.3),
-        K.layers.Dense(10, activation='softmax')
-    ])
+    inputs = K.Input(shape=(32, 32, 3))
+    resize = K.layers.Lambda(lambda image: K.backend.resize_images(image, height_factor=3, width_factor=3,
+                                                                    data_format='channels_last', interpolation='bilinear'))(inputs)
+    preprocessed = K.applications.mobilenet_v2.preprocess_input(resize)
+
+    x = base_model(preprocessed, training=False)
+    x = K.layers.Dense(128, activation='relu')(x)
+    x = K.layers.Dropout(0.3)(x)
+    outputs = K.layers.Dense(10, activation='softmax')(x)
+
+    model = K.Model(inputs=inputs, outputs=outputs)
 
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(
-        train_gen,
-        validation_data=val_gen,
-        epochs=5,
-        verbose=1
-    )
+    model.fit(X_train, Y_train,
+              validation_split=0.1,
+              epochs=5,
+              batch_size=64,
+              verbose=1)
 
     model.save('cifar10.h5')
