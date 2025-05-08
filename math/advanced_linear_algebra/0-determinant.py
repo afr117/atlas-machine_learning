2f3c2d74d6568b0a36b9bd3d91def2a66ccccbcd
afr117 <54492982+afr117@users.noreply.github.com>
2025-05-08 17:25:40 -0400

    Update 0-determinant.py

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -19,8 +19,8 @@ def determinant(matrix):
         ValueError: If matrix is not square.
     """
     # Type and shape validation
-    if not isinstance(matrix, list)
-    or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance
+                                               (row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     if matrix == [[]]:
