e915be1ba5f5685bb472be16cea12f28ce8d74db
afr117 <54492982+afr117@users.noreply.github.com>
2025-05-08 17:41:42 -0400

    Update 2-cofactor.py

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -29,6 +29,7 @@ def determinant(matrix):
         det += sign * matrix[0][col] * determinant(sub_matrix)
     return det
 
+
 def cofactor(matrix):
     """
     Calculates the cofactor matrix of a square matrix.
@@ -43,8 +44,8 @@ def cofactor(matrix):
         TypeError: If input is not a list of lists.
         ValueError: If input is not a non-empty square matrix.
     """
-    if not isinstance(matrix, list) or not all(isinstance(
-        row, list) for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+                                               for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     size = len(matrix)
