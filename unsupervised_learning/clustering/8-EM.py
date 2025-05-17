afe9b9220e9b8c9e1d81a6ae09e4cbb6d4b33200
afr117 <54492982+afr117@users.noreply.github.com>
2025-05-16 20:14:31 -0400

    Update 8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -4,7 +4,7 @@ import numpy as np
 
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
-maximization = __import__('7-maximization').maximization
+maximization = __import__('maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
