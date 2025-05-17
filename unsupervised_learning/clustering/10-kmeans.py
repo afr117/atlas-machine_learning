cf3a59a677c853db90bb4379cb026b5d2ea16b98
afr117 <54492982+afr117@users.noreply.github.com>
2025-05-16 20:36:26 -0400

    Update 10-kmeans.py

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -13,7 +13,8 @@ def kmeans(X, k):
 
     Returns:
     - C: np.ndarray of shape (k, d), centroid coordinates
-    - clss: np.ndarray of shape (n,), index of the cluster each point belongs to
+    - clss: np.ndarray of shape (n,), index of the
+    cluster each point belongs to
     """
     model = sklearn.cluster.KMeans(n_clusters=k, n_init='auto')
     model.fit(X)
