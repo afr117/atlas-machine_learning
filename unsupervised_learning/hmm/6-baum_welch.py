6f90087cb65fe73b916dff596b66480355d13315
afr117 <54492982+afr117@users.noreply.github.com>
2025-05-22 10:32:29 -0400

    Update 6-baum_welch.py

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -11,8 +11,8 @@ def forward(Observation, Emission, Transition, Initial):
 
     for t in range(1, T):
         for j in range(N):
-            F[j, t] = np.sum(F[:, t - 1] * Transition[:, j]) * \
-                Emission[j, Observation[t]]
+            F[j, t] = np.sum(F[:, t - 1] * Transition[:, j]) * Emission[j, Observation[t]]
+
     P = np.sum(F[:, -1])
     return P, F
 
@@ -25,8 +25,8 @@ def backward(Observation, Emission, Transition, Initial):
 
     for t in range(T - 2, -1, -1):
         for i in range(N):
-            B[i, t] = np.sum(Transition[i] * Emission[:, Observation[t + 1]] *
-                             B[:, t + 1])
+            B[i, t] = np.sum(Transition[i] * Emission[:, Observation[t + 1]] * B[:, t + 1])
+
     P = np.sum(Initial.T * Emission[:, Observation[0]] * B[:, 0])
     return P, B
 
@@ -48,17 +48,14 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
 
         xi = np.zeros((N, N, T - 1))
         for t in range(T - 1):
-            denom = np.sum(F[:, t] * np.dot(Transition,
-                                            Emission[:, Observations[t + 1]] *
-                                            B[:, t + 1]))
+            denom = np.dot(F[:, t], np.dot(Transition, Emission[:, Observations[t + 1]] * B[:, t + 1]))
             for i in range(N):
-                numer = F[i, t] * Transition[i] * \
-                        Emission[:, Observations[t + 1]] * B[:, t + 1]
+                numer = F[i, t] * Transition[i] * Emission[:, Observations[t + 1]] * B[:, t + 1]
                 xi[i, :, t] = numer / denom
 
         gamma = np.sum(xi, axis=1)
-        prod = (F * B)
-        gamma = np.hstack((gamma, np.sum(prod[:, -1:], axis=0, keepdims=True)))
+        gamma_last = np.sum(F[:, -1] * B[:, -1], keepdims=True)
+        gamma = np.hstack((gamma, gamma_last.reshape(-1, 1)))
 
         Transition = np.sum(xi, axis=2) / np.sum(gamma[:, :-1], axis=1, keepdims=True)
 
