5212780baaa41692a64bbbb940a2e2bf6f912889
afr117 <54492982+afr117@users.noreply.github.com>
2025-06-23 22:16:32 -0400

    Update 4-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -47,7 +47,7 @@ class BayesianOptimization:
 
         Returns:
             X_next (np.ndarray of shape (1,)): Next best sample point.
-            EI (np.ndarray of shape (ac_samples,)): 
+            EI (np.ndarray of shape (ac_samples,)):
             Expected improvement values.
         """
         mu, sigma = self.gp.predict(self.X_s)
