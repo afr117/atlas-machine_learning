ed2db23553d835dc7a58709e1e32195e74d8f2df
Alfred rosado <54492982+afr117@users.noreply.github.com>
2025-10-09 10:04:00 -0400

    Update 1-gp.py

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -46,8 +46,7 @@ class GaussianProcess:
         Returns:
             np.ndarray: Covariance kernel matrix of shape (m, n).
         """
-        sqdist = np.sum(X1**2, 1).reshape(-1, 1) \
-               + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) \ + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
 
     def predict(self, X_s):
@@ -60,7 +59,8 @@ class GaussianProcess:
         Returns:
             tuple: mu, sigma
                 - mu (np.ndarray of shape (s,)): Mean for each point in X_s
-                - sigma (np.ndarray of shape (s,)): Variance for each point in X_s
+                - sigma (np.ndarray of shape (s,)):
+                Variance for each point in X_s
         """
         K_s = self.kernel(self.X, X_s)
         K_ss = self.kernel(X_s, X_s)
