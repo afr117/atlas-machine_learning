4672a719838a9347da431cd28a3b4c8b382b4a87
Alfred rosado <54492982+afr117@users.noreply.github.com>
2025-11-17 00:00:23 -0500

    Update 0-main.py

--- supervised_learning/qa_bot/0-main.py
+++ supervised_learning/qa_bot/0-main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-question_answer = __import__('0-qa').question_answer
+question_answer = __import__('0-qa').question_answer # Imports the function
 
 with open('ZendeskArticles/PeerLearningDays.md') as f:
     reference = f.read()
 
+# The print statement executes the function and displays its return value.
 print(question_answer('When are PLDs?', reference))
